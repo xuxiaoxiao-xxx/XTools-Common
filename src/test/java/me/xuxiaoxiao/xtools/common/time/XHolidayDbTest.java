4dff1b1f81b30059a35c9686b6f732b28e1ff07d
tinyxu <tinyxu@cisco.com>
2025-06-25 14:53:08 +0800

    1.5.0 test

--- src/test/java/me/xuxiaoxiao/xtools/common/time/XHolidayDbTest.java
+++ src/test/java/me/xuxiaoxiao/xtools/common/time/XHolidayDbTest.java
@@ -1,45 +1,41 @@
 package me.xuxiaoxiao.xtools.common.time;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import me.xuxiaoxiao.xtools.common.XTools;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.StringJoiner;
 
 public class XHolidayDbTest {
 
     @Test
     public void generate() throws Exception {
-        JsonNode jsonNode = new ObjectMapper().readTree(XTools.fileToStr(new File("src/test/resources/holiday.json"), "utf-8"));
-        for (int i = 0; i < jsonNode.size(); i++) {
-            JsonNode yearNode = jsonNode.get(String.valueOf(2000 + i));
-            Short[] yearHolidays = new Short[yearNode.size()];
+        String allYearHolidays = XTools.fileToStr(new File("src/test/resources/holiday.txt"), "utf-8");
+        for (String yearHolidaysStr : allYearHolidays.split("\n")) {
+            if (!XTools.isBlank(yearHolidaysStr)) {
+                int year = Integer.parseInt(yearHolidaysStr.substring(yearHolidaysStr.indexOf("[") + 1, yearHolidaysStr.indexOf("]")).trim());
 
-            int index = 0;
-            Iterator<Map.Entry<String, JsonNode>> iterator = yearNode.fields();
-            while (iterator.hasNext()) {
-                Map.Entry<String, JsonNode> entry = iterator.next();
+                String[] dateTypeStrArr = yearHolidaysStr.substring(yearHolidaysStr.indexOf("]") + 1).split(",");
+                Short[] holidaysArr = new Short[dateTypeStrArr.length];
+                for (int i = 0; i < dateTypeStrArr.length; i++) {
+                    String dateTypeStr = dateTypeStrArr[i];
 
-                int type = entry.getValue().asInt();
+                    String dateStr = dateTypeStr.substring(0, dateTypeStr.indexOf("=")).trim();
+                    String typeStr = dateTypeStr.substring(dateTypeStr.indexOf("=") + 1).trim();
 
-                String monthStr = entry.getKey().substring(0, 2);
-                int month = Integer.parseInt(monthStr.startsWith("0") ? monthStr.substring(1) : monthStr) - 1;
+                    int month = Integer.parseInt(dateStr.startsWith("0") ? dateStr.substring(1, 2) : dateStr.substring(0, 2));
+                    int date = Integer.parseInt(dateStr.substring(2).startsWith("0") ? dateStr.substring(3) : dateStr.substring(2));
+                    int type = Integer.parseInt(typeStr);
 
-                String dayStr = entry.getKey().substring(2);
-                int day = Integer.parseInt(dayStr.startsWith("0") ? dayStr.substring(1) : dayStr) - 1;
+                    holidaysArr[i] = (short) (type << 12 | (month - 1) << 8 | (date - 1));
+                }
+                StringJoiner joiner = new StringJoiner(",", "{", "}");
+                for (Short yh : holidaysArr) {
+                    joiner.add("0x" + String.format("%04x", yh));
+                }
 
-                yearHolidays[index++] = (short) (type << 12 | month << 8 | day);
+                System.out.printf("public static final short[] HOLIDAY_%d = %s;%n", year, joiner);
             }
-            StringJoiner stringJoiner = new StringJoiner(",", "{", "}");
-            for (Short yearHoliday : yearHolidays) {
-                stringJoiner.add("0x" + String.format("%04x", yearHoliday));
-            }
-
-            System.out.printf("public static final short[] HOLIDAY_%d = %s;%n", (2000 + i), stringJoiner);
         }
     }
 }
